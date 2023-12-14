284bd1b28b4c9181358d943652cff3366da9f82e
A4N4 <112816168+A4N4@users.noreply.github.com>
2023-12-15 00:58:39 +0100

    Add previous tasks

---
+++ main.py
@@ -0,0 +1,41 @@
+# Task 1
+
+# num1 = int(input('Enter your first number: '))
+#
+# num2 = int(input('Enter your second number: '))
+#
+# num3 = int(input('Enter your third number: '))
+#
+# total=num1+num2+num3
+#
+# print(f"Your total is {total}")
+
+# Task 2
+
+# diagonal1=int(input('Enter length on diagonal length 1: '))
+#
+# diagonal2=int(input('Enter length on diagonal length 2: '))
+#
+# rhombusArea=(diagonal1*diagonal2)/2
+#
+# print(f"Your rhombus area is {rhombusArea}")
+
+# Task 3
+
+# userNumber=int(input("Enter any 4-digit number: "))
+#
+# num1=userNumber // 1000
+# print(num1)
+#
+# num2=userNumber % 1000 // 100
+# print(num2)
+#
+# num3=userNumber // 10 % 10
+# print(num3)
+#
+# num4=userNumber % 10
+# print(num4)
+#
+# result=num1*num2*num3*num4
+#
+# print(f"Your total result is {result}")
