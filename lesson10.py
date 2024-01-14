d7cd88927d92cfc4b2f7e94ca72c0e6d3712374d
A4N4 <112816168+A4N4@users.noreply.github.com>
2024-01-15 00:57:43 +0100

    Lesson 7

---
+++ lesson10.py
@@ -0,0 +1,52 @@
+# Task 1
+import re
+
+# userHomePhone_number=(input("Enter you number, please: \n"))
+#
+# for items in userHomePhone_number:
+#     if re.match(r'[0-9]',userHomePhone_number) and len(userHomePhone_number) == 7:
+#         print("Number is correct")
+# print("Number entered is wrong!")
+
+# Task 2
+
+# userMobilePhone_number=(input("Enter you number, please: \n"))
+#
+# for items in userMobilePhone_number:
+#     userMobilePhone_number.lstrip()
+#     if re.match(r'[+]?[0-9]', userMobilePhone_number) and len(userMobilePhone_number) == 13:
+#         print("Number is correct")
+# print("Number entered is wrong!")
+
+# Task 3
+
+# userEmail=(input("Enter you email, please: \n"))
+#
+# for item in userEmail:
+#     userEmail.lstrip()
+#     if re.match(r'[a-zA-Z0-9]+[.]?[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]{12,20}', userEmail):
+#         print("Your email is correct.")
+#     else:
+#         print("Your email is wrong!")
+
+# Task 4
+# userPIB=(input("Vedit', bud laska, PIB: \n"))
+#
+# for items in userPIB:
+#     if re.match(r'[a-zA-Z]{1,19}+\s+[a-zA-Z]{1,19}+\s+[a-zA-Z]{1,19}', userPIB):
+#         print(f"Your PIB is {userPIB}")
+#     else:
+#         print("Value entered incorrect")
+
+# Password
+# userPassword=input("Enter your password: ")
+# for signs in userPassword:
+#     userPassword.lstrip()
+#     if re.match(r'^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z!@$*%#^]{8,16}$', userPassword):
+#         print("Your password is correct: ")
+#     else:
+#         print("Password is incorrect")
+
+
+
+
