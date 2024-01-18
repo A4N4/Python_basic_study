56bd50db91e6861d792a6c805bab5bcfbff51f4d
A4N4 <112816168+A4N4@users.noreply.github.com>
2024-01-18 20:34:45 +0100

    Lesson 7

---
+++ Lesson11.py
@@ -0,0 +1,34 @@
+
+
+# Task 1
+# import re
+#
+#
+# with open ("test_text.txt",'r') as test:
+#     text=test.read().split()
+#     for i in text:
+#         if re.match(r'^[a-zA-ZА-Яа-яёЁЇїІіЄєҐґ]{7}+$', i):
+#             print(i)
+
+# Task 2
+
+# with open ("test_text.txt",'r') as test:
+#     newtezt=test.read().split()
+#     count=0
+#     for i in range(len(newtezt)):
+#         count+=1
+#     print(count)
+# print(newtezt)
+
+# Task 3
+# import re
+# new_text=''
+#
+# with open ("test_text.txt",'r') as test:
+#     for i in test:
+#         new_text+=i
+#     # print(new_text)
+# updatedText, local =re.subn(r'praesentium','**********', new_text)
+# print(updatedText, local)
+
+
